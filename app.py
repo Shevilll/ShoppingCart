e7ce4f84873187665ce20bdc91149f5d20eb0785
Shevilll <ahmadfaraz00710@gmail.com>
2023-04-27 17:56:07 +0530

    Function working

--- app.py
+++ app.py
@@ -2,24 +2,29 @@ from flask import Flask,render_template,request
 from sqlite3 import connect
 
 app = Flask(__name__)
-db = connect("data.db",check_same_thread=False)
+db = connect("cart.db",check_same_thread=False)
 cur = db.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS data(no int,name varchar(20))")
+cur.execute("CREATE TABLE IF NOT EXISTS data(Item varchar(50),Amount int,Price float)")
 db.commit()
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/data",methods=["POST","GET"])
-def data():
+@app.route("/add",methods=["POST","GET"])
+def add():
     name = request.form.get("name")
-    if name == "" or name == None:
-        data = cur.execute("select * from data")
-        return render_template("data.html",data=data)
-    cur.execute(f'INSERT INTO data VALUES(1,"{name}")')
+    amount = request.form.get("amount")
+    price = request.form.get("price")
+    cur.execute(f'INSERT INTO data VALUES(?,?,?)',(name,amount,price))
     db.commit()
+    return render_template("index.html")
+
+@app.route("/data")
+def data():
     data = cur.execute("select * from data")
     return render_template("data.html",data=data)
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
