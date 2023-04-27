b058c75cd57664602f75275cbebd2e9de3c98468
Shevilll <ahmadfaraz00710@gmail.com>
2023-04-27 21:00:07 +0530

    cart done

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 db = connect("cart.db",check_same_thread=False)
 cur = db.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS data(Item varchar(50),Amount int,Price float)")
+cur.close()
 db.commit()
 
 @app.route("/")
@@ -13,17 +14,39 @@ def home():
 
 @app.route("/add",methods=["POST","GET"])
 def add():
+    cur = db.cursor()
     name = request.form.get("name")
     amount = request.form.get("amount")
     price = request.form.get("price")
     cur.execute(f'INSERT INTO data VALUES(?,?,?)',(name,amount,price))
     db.commit()
+    cur.close()
     return render_template("index.html")
 
 @app.route("/data")
 def data():
+    cur = db.cursor()
     data = cur.execute("select * from data")
-    return render_template("data.html",item=data.fetchall())
+    data = data.fetchall()
+    cur.close()
+    return render_template("data.html",item=data)
+
+@app.route("/remove/<item>",methods=["POST","GET"])
+def remove(item):
+    cur = db.cursor()
+    items = item.split(",")
+    print(items)
+    cur.execute("DELETE FROM data WHERE item=? and amount=? and price=?",(items[0],int(items[1]),int(float(items[2]))))
+    db.commit()
+    cur.close()
+
+
+    cur = db.cursor()
+    data = cur.execute("select * from data")
+    data = data.fetchall()
+    cur.close()
+    return render_template("data.html",item=data)
+
 
 
 if __name__ == "__main__":
